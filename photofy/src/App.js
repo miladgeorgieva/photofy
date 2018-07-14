562a92a663015719bea6c2f1f9bd6dabe8497583
Mila Hristova <hristova.mila@abv.bg>
2018-07-14 23:48:51 +0300

    Beta version 0.1

--- photofy/src/App.js
+++ photofy/src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+import HomePage from './components/homepage/HomePage';
+import CreateArticlePage from './components/article/CreateArticlePage';
+import EditArticlePage from './components/article/EditArticlePage';
+import FullArticlePage from './components/article/FullArticlePage';
+import ProfilePage from './components/user/ProfilePage';
+import LikedPostsPage from './components/user/LikedPostsPage';
+import SearchResultsPage from './components/common/SearchResultsPage';
+import AdminPage from './components/admin/AdminPage';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Logout from './components/user/Logout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import RedirectRoute from './components/common/RedirectRoute'
+import GuestRoute from './components/common/GuestRoute'
+import AdminRoute from './components/common/AdminRoute'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="body-container">
+        <Router>
+          <Switch>
+            <GuestRoute path="/" exact component={Login}/>
+            <GuestRoute path="/register" component={Register}/>
+            <RedirectRoute path="/logout" exact component={Logout}/>
+            <RedirectRoute path="/home" component={HomePage}/>
+            <RedirectRoute path="/create-post" component={CreateArticlePage}/>
+            <RedirectRoute path="/article/:id" component={FullArticlePage}/>
+            <RedirectRoute path="/edit/:id" component={EditArticlePage}/>
+            <RedirectRoute path="/user/:id" component={ProfilePage}/>
+            <RedirectRoute path="/liked-posts" component={LikedPostsPage}/>
+            <RedirectRoute path="/search-results" component={SearchResultsPage}/>
+            <AdminRoute path="/admin/users" component={AdminPage}/>
+          </Switch>
+        </Router>
+        <ToastContainer/>
       </div>
-    );
+    )
   }
 }
 
